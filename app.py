b9e26d7a6da52c04c83856890822d4c1aef9c8ae
Santosh Kumar Prasad <thechampsant@gmail.com>
2019-06-24 03:27:37 +0800

    hh

--- app.py
+++ app.py
@@ -2,22 +2,13 @@ import os
 from flask import Flask, request
 from collections import defaultdict
 import json    
-import pymongo
-
 app = Flask(__name__)
 
-app.config["MONGO_URI"]="mongodb://heroku_pqsw7hfs:n9ebtdb84uiv8ugh3juo2hamqr@ds241977.mlab.com:41977/heroku_pqsw7hfs"
+
 
 @app.route("/")
 def hello():
     return "Hello from Python!"
-
-mongo = PyMongo(app) 
-
-with open('result.json') as f:
-    file_data = json.load(f)
-    
-myList = []
 
 
 
