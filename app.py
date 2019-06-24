1b7c259ab9a3b75bb1b4f0d3307b7c24a0355d87
Santosh Kumar Prasad <thechampsant@gmail.com>
2019-06-24 12:27:09 +0800

    mongoc

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def hello():
     if file_data:
         return json.dumps(file_data)
     
-    sensor = db.sensorList
+    sensor = db.file_data
     sensor_id = posts.insert_one(sensor).inserted_id
         
     return 'ok'
